a03e464c3aad83fcabafc212068f38a10570369c
Siddharth Pande <sidarjunstn.sp@gmail.com>
2022-05-09 22:54:00 +0530

    Project Initialized with webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resourceUsage } = require('process');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/app.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugin: [
+        new HtmlWebpackPlugin({
+            title: 'do whats to-do'
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ott)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
