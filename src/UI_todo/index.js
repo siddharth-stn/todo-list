ca3bef6553f55fc99702450754f72ce4c10f8437
Siddharth Pande <sidarjunstn.sp@gmail.com>
2022-05-26 20:30:58 +0530

    latest code version

--- src/UI_todo/index.js
+++ src/UI_todo/index.js
@@ -1,4 +1,5 @@
 import styles from  './index.module.scss';
+
 export default function () {
     function createDiv () {
         return document.createElement('div');
@@ -27,33 +28,134 @@ export default function () {
 
 
     const body = document.querySelector('body');
-    const h1 = document.createElement('h1');
+    
     const button = document.createElement('button');
+    
+    
+    
+    // Container
     const containerDiv = createDiv();
+    containerDiv.classList.add(styles.containerDiv);
+
+    // Header 
+    const h1 = document.createElement('h1');
     const headerDiv = createDiv();
-    const leftSideDiv = createDiv();
-    const rightSideDiv = createDiv();
-    const leftSideHeading = createH2();
-    const rightSideHeading = createH2();
-    const rightSideDivTop = createDiv();
-
     h1.innerText = 'WHAT TO-DO';
-    h1.classList.add(styles.blue);
+    headerDiv.classList.add(styles.headerDiv);
     headerDiv.appendChild(h1);
+    
+
+    // Main Content
+    const mainDiv = createDiv();
+    mainDiv.classList.add(styles.mainDiv);
+
+        // Left Side Div
+        const leftSideDiv = createDiv();
+        const leftSideHeading = createH2();
+        leftSideHeading.innerText = 'Projects';
+        leftSideHeading.classList.add(styles.leftSideHeading);
+        leftSideDiv.classList.add(styles.leftSideDiv);
+        leftSideDiv.appendChild(leftSideHeading);
+
+        // Right Side Div
+        const rightSideDiv = createDiv();
+        const rightSideHeading = createH2();
+        rightSideHeading.innerText = 'Project One Name';
+        rightSideHeading.classList.add(styles.rightSideHeading);
+        rightSideDiv.classList.add(styles.rightSideDiv);
+        rightSideDiv.appendChild(rightSideHeading);
+
+            // Right container div
+            const rightContainer = createDiv();
+            rightContainer.classList.add(styles.rightContainer);
+
+                // Task Container
+                const taskContainer = createDiv();
+                taskContainer.classList.add(styles.taskContainer);
+
+                // Form Container
+                const form = document.createElement('form');
+
+                    // Input Container
+                    const inputContainer = createDiv();
+                    inputContainer.classList.add(styles.inputContainer);
+                    const enterTask = createInput();
+                    enterTask.placeholder = "write your new task here...";
+                    enterTask.classList.add(styles.inputBox);
+                    const enterDate = createInput();
+                    enterDate.placeholder = "enter due date...";
+                    enterDate.classList.add(styles.inputBox);
+                    const enterTaskDescription = createInput();
+                    enterTaskDescription.placeholder = "write task description...";
+                    enterTaskDescription.classList.add(styles.inputBox);
+                        
+
+                    // Radio Container
+                    const radioContainer = createDiv();
+                    radioContainer.classList.add(styles.radioContainer);
+                    const highLabel = document.createElement('label');
+                    highLabel.textContent = "High Priority";
+                    highLabel.style.color = "red";
+                    const lowLabel = document.createElement('label');
+                    lowLabel.textContent = "Low Priority";
+                    lowLabel.style.color = "#FDE16C";
+                    const highRadioDiv = createDiv();
+                    highRadioDiv.classList.add(styles.highRadioDiv);
+                    const lowRadioDiv = createDiv();
+                    lowRadioDiv.classList.add(styles.lowRadioDiv);
+                    const highRadio = createInput();
+                    highRadio.type = "radio";
+                    highRadio.name = "priority";
+                    const lowRadio = createInput();
+                    lowRadio.type = "radio";
+                    lowRadio.name = "priority"
+
+                    
+                    // Button
+                    const newTaskBtn = document.createElement('button');
+                    newTaskBtn.classList.add(styles.newTaskBtn);
+                    newTaskBtn.textContent = "Add New Task";
+                    
+            // Append Elements to right container div and its children-------->
+                    // Append child to input container
+                    inputContainer.appendChild(enterTask);
+                    inputContainer.appendChild(enterDate);
+                    inputContainer.appendChild(enterTaskDescription);
+
+                    // Append Child to radio container div
+                        // Append child to high radio div
+                        highRadioDiv.appendChild(highLabel);
+                        highRadioDiv.appendChild(highRadio);
+                        
+                        // Append child to low radio div    
+                        lowRadioDiv.appendChild(lowLabel);
+                        lowRadioDiv.appendChild(lowRadio);
+                                
+                    radioContainer.appendChild(highRadioDiv);
+                    radioContainer.appendChild(lowRadioDiv);
+                
+                // Append child to form             
+                form.appendChild(inputContainer);
+                form.appendChild(radioContainer);
+                form.appendChild(newTaskBtn);
+
+            // Append Child to right container div    
+            rightContainer.appendChild(taskContainer);
+            rightContainer.appendChild(form);
+
+        // Append child to right side div
+        rightSideDiv.appendChild(rightContainer);
+
+    // Append child to Main Content Div
+    mainDiv.appendChild(leftSideDiv);
+    mainDiv.appendChild(rightSideDiv);
+
+// Append child to container div
+containerDiv.appendChild(headerDiv);
+containerDiv.appendChild(mainDiv);
 
 
-    leftSideHeading.innerText = 'Projects';
-    leftSideDiv.appendChild(leftSideHeading);
 
-    rightSideHeading.innerText = 'Project One Name'
-    rightSideDivTop.appendChild(rightSideHeading);
-    rightSideDiv.appendChild(rightSideDivTop);
-
-
-
-    containerDiv.appendChild(headerDiv);
-    containerDiv.appendChild(leftSideDiv);
-    containerDiv.appendChild(rightSideDiv);
-   
-    body.appendChild(containerDiv);
+// Append child to body
+body.appendChild(containerDiv);
 }
